7b83cda5726b3c1361be9ff914918d9b0c5ece7d
Andrew Harris <arharris@mun.ca>
2025-10-15 12:23:00 +0900

    Command line arguments

--- main.cpp
+++ main.cpp
@@ -1,10 +1,19 @@
+#include <iostream>
 #include <string>
 
 #include "Simulation.h"
 
-int main()
+int main(int argc, char* argv[])
 {
-    Simulation data = Simulation(".//tests//files//hydro32_00020.csv");
+    if (argc < 3) {
+        std::cerr << "Usage: " << argv[0] << " particle_file config_file" << std::endl;
+    }
+    Simulation data = Simulation(argv[1]);
+    data.useConfig(argv[2]);
+
+    data.densityIterate();
+
+    std::cout << "Density iteration complete" << std::endl;
 
     return 0;
 }
